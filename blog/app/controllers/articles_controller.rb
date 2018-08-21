e9749b9b51e5214f387b501f57293b37ec968c40
lstravers <stravers222@gmail.com>
2018-08-20 23:13:37 -0400

    Added files. Stopped at section 5.6 Saving data in the controller of Getting Started with Rails guide ffor clarity on adding correct code to articles_controller.rb.

--- blog/app/controllers/articles_controller.rb
+++ blog/app/controllers/articles_controller.rb
@@ -3,6 +3,23 @@ class ArticlesController < ApplicationController
     end
 
     def create
-        render plain: params[:article].inspect
+				@article = Article.new(params.require(:article).permit(:title, :text))
+				
+				#Article.new(params[:article])
+       
+        @article.save
+        redirect_to @article
+		end
+		
+				@article = Article.new(params.require(:article).permit(:title, :text))
+
+
+    def show
+      @article = Article.find(params[:id])
     end
+     
+    def new
+    end
+     
+    # snippet for brevity
 end
